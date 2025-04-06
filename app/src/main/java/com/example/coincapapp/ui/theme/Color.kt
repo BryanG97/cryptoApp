07b75410150a0b8731afdc75b31a2b7dc64db9c5
Bryan Guevara <bguevara@ec.krugercorp.com>
2025-04-05 20:37:36 -0500

    Cambio version de Android y construcción de proyecto

--- app/src/main/java/com/example/coincapapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/coincapapp/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.coinapp.ui.theme
+package com.example.coincapapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/com/example/coincapapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/coincapapp/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.coinapp.ui.theme
+package com.example.coincapapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
